fc44c2eaef52966d9aeac589a9da10098c800ca5
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-22 08:59:29 +0700

    object

--- arrow_function.js
+++ arrow_function.js
@@ -1,4 +1,12 @@
-const perkalian5 = (num) => 5 * num;
+const perkalian2 = (num) => 2 * num;
 
-const nih = perkalian5(20);
+const nih = perkalian2(20);
 console.log(nih);
+
+function perkalian5(num = 5) {
+  const hasil = 5 * num;
+
+  console.log(`Hasil perkalian 5 x ${num} =`, hasil);
+}
+
+perkalian5(4);
---
+++ object.js
@@ -0,0 +1,28 @@
+// Object adalah kumpulan pasangan key-value dan merupakan tipe data yang bukan primitif.
+const user = {
+  name: "Ukhasyah",
+  country: "Indonesia",
+  age: 17,
+};
+
+// 3 Cara akses Propery di Object
+console.log(user);
+console.log(user.name);
+console.log(user["country"]);
+
+const { name, country } = user;
+console.log(name, country);
+
+// Ubah nilai properti pada Object
+const Account = {
+  saldo: 2000,
+};
+
+
+Account.saldo = 2000000;
+console.log(Account.saldo);
+
+// 2 cara hapus properti pada object
+delete Account.saldo;
+delete Account['saldo']
+console.log(Account.saldo);
