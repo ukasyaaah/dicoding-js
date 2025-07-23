e9356cad20aa7ffef7e88462ace5e16b557bf45f
Ukhasyah Ganteng <ukasyaaah@gmail.com>
2025-07-23 08:35:40 +0700

    encapsulation

---
+++ oop/encapsulation.js
@@ -0,0 +1,31 @@
+/*
+- proses untuk membungkus data 
+  di suatu wadah yang disebut dengan class.
+
+- Menyembunyikan data adalah 
+  bagian kunci dari encapsulation.
+
+- Karena kita menerapkan encapsulation, 
+  kita harus mengatur akses properti & method,
+  kita dapat menerapkan getter dan setter.
+*/
+
+class Tiket {
+  #jadwal;
+  constructor(tujuan, jadwal) {
+    this.tujuan = tujuan;
+    this.#jadwal = jadwal;
+  }
+
+  set jadwal(jadwal) {
+    console.log("Anda tidak diperbolehkan mengubah jadwal");
+  }
+
+  get jadwal() {
+    return this.#jadwal;
+  }
+}
+
+let tiket = new Tiket("Jakarta", "Senin");
+
+console.log(tiket);
